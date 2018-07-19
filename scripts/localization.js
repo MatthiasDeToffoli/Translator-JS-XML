30ef2a9f797e4f9542bc4d443f18d9433726b9bf
MatthiasDeToffoli <matthias.detoffoli@gmail.com>
2018-07-19 14:54:49 +0200

    [ADD] first code

---
+++ scripts/localization.js
@@ -0,0 +1,65 @@
+const FR_XML_PATH = "./xml/fr.xml";
+const EN_XML_PATH = "./xml/en.xml";
+const DEFAULT_LANGUAGE = "FR";
+const FR = "FR";
+const EN = "EN";
+
+var frXML, enXML;
+var xmlhttpFR = new XMLHttpRequest();
+
+  xmlhttpFR.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      frXMLGet(this);
+    }
+  };
+
+    var xmlhttpEN = new XMLHttpRequest();
+
+      xmlhttpEN.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          enXMLGet(this);
+        }
+      };
+  $(document).ready(function(){
+    xmlhttpFR.open("GET", FR_XML_PATH, true);
+    xmlhttpFR.send();
+    xmlhttpEN.open("GET", EN_XML_PATH, true);
+    xmlhttpEN.send();
+  });
+
+
+  function frXMLGet(xml) {
+    frXML = xml.responseXML;
+
+    if(DEFAULT_LANGUAGE === FR)
+      Translate(FR);
+  }
+
+
+
+
+    function enXMLGet(xml) {
+      enXML = xml.responseXML;
+      if(DEFAULT_LANGUAGE === EN)
+      Translate(EN);
+    }
+
+function Translate(language){
+  var xml;
+  switch (language) {
+    case "FR":
+      xml = frXML;
+      break;
+    case "EN":
+      xml = enXML;
+      break;
+    default:
+      xml = frXML;
+  }
+
+  $(".Translatable").each(function(){
+    var xmlElement = xml.getElementsByTagName($(this).attr("key"));
+    if(xmlElement != null && xmlElement.length > 0)
+      this.innerHTML = xmlElement[0].innerHTML;
+  });
+}
